723d1d8c0a7dd253c70945ea0e179aadeb286da1
Aakash Jena <63442481+ash-295@users.noreply.github.com>
2020-08-29 16:28:05 +0530

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -216,49 +216,38 @@
 	}
 
 	/*-------------------- COUNTDOWN -------------------*/
-	if( $( '#clockdiv' )[0] ){
-		function getTimeRemaining(endtime) {
-			var t = Date.parse(endtime) - Date.parse(new Date());
-			var seconds = Math.floor((t / 1000) % 60);
-			var minutes = Math.floor((t / 1000 / 60) % 60);
-			var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-			var days = Math.floor(t / (1000 * 60 * 60 * 24));
-			return {
-				'total': t,
-				'days': days,
-				'hours': hours,
-				'minutes': minutes,
-				'seconds': seconds
-			};
-		}
-
-		function initializeClock(id, endtime) {
-			var clock = document.getElementById(id);
-			var daysSpan = clock.querySelector('.days');
-			var hoursSpan = clock.querySelector('.hours');
-			var minutesSpan = clock.querySelector('.minutes');
-			var secondsSpan = clock.querySelector('.seconds');
-
-			function updateClock() {
-				var t = getTimeRemaining(endtime);
-
-				daysSpan.innerHTML = t.days;
-				hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-				minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-				secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-				if (t.total <= 0) {
-					clearInterval(timeinterval);
-				}
-			}
-
-			updateClock();
-			var timeinterval = setInterval(updateClock, 1000);
-		}
-
-		var deadline = new Date(Date.parse(new Date()) + 31 * 24 * 60 * 60 * 1000);
-		initializeClock('clockdiv', deadline);
-	}
+	var countDownDate = new Date("Sept 6, 2020 11:00:00").getTime();
+      
+      // Update the count down every 1 second
+      var x = setInterval(function() {
+      
+        // Get today's date and time
+        var now = new Date().getTime();
+          
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+          
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+          
+        // Output the result in an element with id="demo"
+        document.getElementById("days").innerHTML = days ;
+        document.getElementById("hours").innerHTML = hours ;
+        document.getElementById("minutes").innerHTML = minutes ;
+        document.getElementById("seconds").innerHTML = seconds ;
+          
+        // If the count down is over, write some text 
+        if (distance < 0) {
+          clearInterval(x);
+          document.getElementById("days").innerHTML = "00" ;
+        document.getElementById("hours").innerHTML = "00" ;
+        document.getElementById("minutes").innerHTML = "00" ;
+        document.getElementById("seconds").innerHTML = "00" ;
+        }
+      }, 1000);
 
 	/* ------------- SCHEDULE MARATHON TABS ------------- */
 	if ( $('.schedule-item-info')[0] ) {
